92c239c1cd047a652f21495cb9e100721a111c9b
MaxPlummer <108943512+MaxPlummer@users.noreply.github.com>
2023-08-04 21:40:05 -0400

    Update main.cpp
    All fixes done and both searches added

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <set>
 #include <algorithm>
 #include <map>
+#include <queue>
+#include <stack>
 
 class Airline {
 public:
@@ -38,7 +40,7 @@ private:
             return code;
         }
 
-        void addAirline(std::string airlineName) {
+        void addAirline(std::string airlineName, int statMinutesDelayedTotal) {
             // Check if the airline already exists in the map
             auto it = airlines.find(airlineName);
             if (it == airlines.end()) {
@@ -48,11 +50,21 @@ private:
             }
 
             // Update the total minutes delayed for the airline
+            it->second.addMinutesDelayed(statMinutesDelayedTotal);
+        }
+
+        int getTotalMinutesDelayed() const {
+            int totalMinutesDelayed = 0;
+            for (const auto& airlinePair : airlines) {
+                totalMinutesDelayed += airlinePair.second.getTotalMinutesDelayed();
+            }
+            return totalMinutesDelayed;
         }
 
         const std::map<std::string, Airline>& getAirlines() const {
             return airlines;
         }
+
 
         std::shared_ptr<AirportNode> left;
         std::shared_ptr<AirportNode> right;
@@ -69,12 +81,28 @@ public:
         root = insertRecursive(root, airportCode);
     }
 
-    void addAirlineToAirport(std::string airportCode, std::string airlineName) {
-        addAirlineToAirportRecursive(root, airportCode, airlineName);
+    void addAirlineToAirport(std::string airportCode, std::string airlineName, int statMinutesDelayedTotal) {
+        addAirlineToAirportRecursive(root, airportCode, airlineName, statMinutesDelayedTotal);
     }
 
     void printInOrder() const {
         printInOrderRecursive(root);
+    }
+
+    std::map<std::string, int> getTotalMinutesDelayedByAirline() const {
+        std::map<std::string, int> airlineTotalMinutes;
+
+        getTotalMinutesDelayedRecursive(root, airlineTotalMinutes);
+
+        return airlineTotalMinutes;
+    }
+
+    void BFS(const std::string& code) {
+        helperBFS(code);
+    }
+
+    void DFS(const std::string& code) {
+        helperDFS(code);
     }
 
 private:
@@ -95,14 +123,14 @@ private:
         return currentNode;
     }
 
-    void addAirlineToAirportRecursive(std::shared_ptr<AirportNode> currentNode, std::string airportCode, std::string airlineName) {
+    void addAirlineToAirportRecursive(std::shared_ptr<AirportNode> currentNode, std::string airportCode, std::string airlineName, int statMinutesDelayedTotal) {
         if (currentNode != nullptr) {
             if (currentNode->getCode() == airportCode) {
-                currentNode->addAirline(airlineName);
+                currentNode->addAirline(airlineName, statMinutesDelayedTotal);
             } else if (airportCode < currentNode->getCode()) {
-                addAirlineToAirportRecursive(currentNode->left, airportCode, airlineName);
+                addAirlineToAirportRecursive(currentNode->left, airportCode, airlineName, statMinutesDelayedTotal);
             } else {
-                addAirlineToAirportRecursive(currentNode->right, airportCode, airlineName);
+                addAirlineToAirportRecursive(currentNode->right, airportCode, airlineName, statMinutesDelayedTotal);
             }
         }
     }
@@ -110,13 +138,115 @@ private:
     void printInOrderRecursive(std::shared_ptr<AirportNode> currentNode) const {
         if (currentNode != nullptr) {
             printInOrderRecursive(currentNode->left);
-            std::cout << "Airport: " << currentNode->getCode() << std::endl << " Airlines: ";
+            std::cout << "Airport: " << currentNode->getCode() << std::endl << "Airlines: ";
             for (const auto& airlinePair : currentNode->getAirlines()) {
-                std::cout << airlinePair.first << ", ";
+                std::cout << airlinePair.first << "\"" << ", ";
             }
+            int airportTotalMinutedDelayed = 0;
+            for (const auto& airlinePair : currentNode->getAirlines()) {
+                airportTotalMinutedDelayed += airlinePair.second.getTotalMinutesDelayed();
+            }
+            std::cout << std::endl;
+            std::cout << "Total Minutes Delayed for the Airport: " << airportTotalMinutedDelayed << std::endl;
             std::cout << std::endl;
             printInOrderRecursive(currentNode->right);
         }
+    }
+
+    void getTotalMinutesDelayedRecursive(std::shared_ptr<AirportNode> currentNode, std::map<std::string, int>& airlineTotalMinutes) const {
+        if (currentNode != nullptr) {
+            const std::map<std::string, Airline>& airlines = currentNode->getAirlines();
+            for (const auto& airlinePair : airlines) {
+                std::string airlineName = airlinePair.first;
+                int minutesDelayed = airlinePair.second.getTotalMinutesDelayed();
+
+                // Check if the airline already exists in the map
+                auto it = airlineTotalMinutes.find(airlineName);
+                if (it == airlineTotalMinutes.end()) {
+                    // If the airline does not exist, create a new entry with total minutes delayed set to 0
+                    airlineTotalMinutes[airlineName] = 0;
+                    it = airlineTotalMinutes.find(airlineName); // Update the iterator after emplacing the new entry
+                }
+
+                // Update the total minutes delayed for the airline
+                it->second += minutesDelayed;
+            }
+
+            getTotalMinutesDelayedRecursive(currentNode->left, airlineTotalMinutes);
+            getTotalMinutesDelayedRecursive(currentNode->right, airlineTotalMinutes);
+        }
+    }
+
+    void helperBFS(const std::string& code) {
+        // Check that there is an existing root
+        if (root == nullptr) {
+            return;
+        }
+
+        // Create queue for visiting tree nodes
+        std::queue<std::shared_ptr<AirportNode>> q;
+        // Add the root to the queue
+        q.push(root);
+
+        // Iterate through q until empty
+        while (!q.empty()) {
+            // Current node set to node at front of queue
+            std::shared_ptr<AirportNode> currentNode = q.front();
+            // Print data if code is found and return
+            if (currentNode->getCode() == code) {
+                std::cout << "Airport: " << currentNode->getCode() << " found!" << std::endl;
+                std::cout << "Total minutes delayed: " << currentNode->getTotalMinutesDelayed() << std::endl;
+                return;
+            }
+            // Push the left and right child nodes of current node if they exist
+            if (currentNode->left != nullptr) {
+                q.push(currentNode->left);
+            }
+            if (currentNode->right != nullptr) {
+                q.push(currentNode->right);
+            }
+            // Remove current node from the queue
+            q.pop();
+        }
+        std::cout << code << " not found!" << std::endl;
+    }
+
+    void helperDFS(const std::string& code) {
+        //Check if root exists
+        if (root == nullptr) {
+            return;
+        }
+
+        //Create stack for visiting tree nodes
+        std::stack<std::shared_ptr<AirportNode>> stack;
+        // Add the root to the stack
+        stack.push(root);
+
+        while (!stack.empty()) {
+            //Current node set to node at top of stack (peek)
+            std::shared_ptr<AirportNode> currentNode = stack.top();
+            // Remove current node from the stack
+            stack.pop();
+
+            //Prints data if Airport is found and return
+            if (currentNode->getCode() == code) {
+                std::cout << "Airport: " << currentNode->getCode() << " found!" << std::endl;
+                std::cout << "Total minutes delayed: " << currentNode->getTotalMinutesDelayed() << std::endl;
+                return;
+            }
+
+            //Pushes right child node of current node if it exists
+            if (currentNode->right != nullptr) {
+                stack.push(currentNode->right);
+            }
+
+            //Pushes left child node of current node if it exists
+            if (currentNode->left != nullptr) {
+                stack.push(currentNode->left);
+            }
+        }
+
+        std::cout << code << " not found!" << std::endl;
     }
 };
 
@@ -130,24 +260,24 @@ int main() {
         return 1;
     }
 
-    int minutesdelayed;
-
     std::string line;
     std::getline(inputFile, line); // Skip the header line
-
     std::vector<std::string> elements;
     std::vector<int> late;
 
     while (std::getline(inputFile, line)) {
         std::istringstream iss(line);
-        std::string airportCode, carriers;
+        std::string airportCode, carriers, lateAircraftDelays, timeMonth;
+        std::string timeMonthName, timeYear, statDelaysCarrier, statDelaysLateAircraft;
+        std::string statDelaysNAS, statDelaysSecurity, statDelaysWeather, statCarriersName, statCarriersTotal;
+        std::string statFlightsCancelled, statFlightsDelayed, statFlightsDiverted, statFlightsOnTime;
+        std::string statFlightsTotal, statMinutesDelayedCarrier, statMinutesDelayedLateAircraft;
+        std::string statMinutesDelayedNAS, statMinutesDelayedSecurity, statMinutesDelayedTotal, statMinDelayedWeather;
 
         // Read the columns from the CSV line
         while (std::getline(iss, airportCode, ',')) {
             while (std::getline(iss, carriers, ',')) {
                 if (std::all_of(carriers.begin(), carriers.end(), ::isdigit)) {
-                    minutesdelayed = std::stoi(carriers);
-                    late.push_back(minutesdelayed);
                     break;
                 } else {
                     elements.push_back(carriers);
@@ -155,10 +285,36 @@ int main() {
             }
             break;
         }
+        std::getline(iss, timeMonth, ',');
+        std::getline(iss, timeMonthName, ',');
+        std::getline(iss, timeYear, ',');
+        std::getline(iss, statDelaysCarrier, ',');
+        std::getline(iss, statDelaysLateAircraft, ',');
+        std::getline(iss, statDelaysNAS, ',');
+        std::getline(iss, statDelaysSecurity, ',');
+        std::getline(iss, statDelaysWeather, ',');
+        while (std::getline(iss, statCarriersName, ',')) {
+            if (std::all_of(statCarriersName.begin(), statCarriersName.end(), ::isdigit)) {
+                break;
+            }
+        }
+        std::getline(iss, statCarriersTotal, ',');
+        std::getline(iss, statFlightsCancelled, ',');
+        std::getline(iss, statFlightsDelayed, ',');
+        std::getline(iss, statFlightsDiverted, ',');
+        std::getline(iss, statFlightsOnTime, ',');
+        std::getline(iss, statFlightsTotal, ',');
+        std::getline(iss, statMinutesDelayedCarrier, ',');
+        std::getline(iss, statMinutesDelayedLateAircraft, ',');
+        std::getline(iss, statMinutesDelayedNAS, ',');
+        std::getline(iss, statMinutesDelayedSecurity, ',');
+        std::getline(iss, statMinutesDelayedTotal, ',');
+        std::getline(iss, statMinDelayedWeather, ',');
+        statMinutesDelayedTotal = statMinutesDelayedSecurity;
 
         airportBST.insertAirport(airportCode);
         for (size_t i = 0; i < elements.size(); ++i) {
-            airportBST.addAirlineToAirport(airportCode, elements[i]);
+            airportBST.addAirlineToAirport(airportCode, elements[i], std::stoi(statMinutesDelayedTotal));
         }
 
         // Clear the vectors for the next iteration
@@ -168,6 +324,13 @@ int main() {
 
     // Print all airports and their airlines
     airportBST.printInOrder();
+    std::string strIn;
+    std::cout << "\nEnter airport code: " << std::endl;
+    std::cin >> strIn;
+    std::cout << "BFS:" << std::endl;
+    airportBST.BFS(strIn);
+    std::cout << "DFS:" << std::endl;
+    airportBST.DFS(strIn);
 
     return 0;
 }
